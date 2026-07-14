7428eddacc095ad4d466d5066fb21828be5ce0dd
Urieel <urielsalazar.dev@gmail.com>
2026-07-14 12:40:37 -0600

    Start to create db tables and add db_url to .env

---
+++ app/database/db.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase
+from dotenv import load_dotenv
+import os 
+
+load_dotenv() 
+
+DATABSE_URL = os.getenv("DATABASE_URL")
+    
+engine = create_engine(
+    URL,echo=True)
+
+
+class Base(DeclarativeBase):
+    pass
+
--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 import uvicorn
+from routers import appointments
 from routers import patients 
 app=FastAPI()
 
 app.include_router(patients.router, prefix="/patients",tags=["patients"])
-
+app.include_router(appointments.router,prefix="/appointments",tags=["appointments"])
