4b59798c8f6b412666eda956fddbe78f3460d8a0
uriel-salazar <urielsalazar.dev@gmail.com>
2026-08-06 09:30:22 -0600

    Refactor appointment router to improve error handling and streamline CRUD operations

--- app/routers/appointments.py
+++ app/routers/appointments.py
@@ -1,28 +1,35 @@
 from fastapi import APIRouter,Depends
+from fastapi.exceptions import HTTPException
 from models.appointment import Appointment
 from schemas import CreateAppointment,UpdateAppointment
-import crud
+from crud.appointments import update_appointment,create_appointment,get_appointment
 from sqlalchemy.orm import Session
 from database.db import get_db
 router = APIRouter()
 
 
 @router.get("/{appointment_id}")
-def get(appointment_id:int,db:Session = Depends(get_db)):
-   return db.query(Appointment).filter(Appointment.appointment_id == appointment_id).first()
+def get(db:Session = Depends(get_db),appointment_id:int):
+   if not appointment_id:
+      raise HTTPException(status_code = 404, detail ='Appointment not found')
+   
+   return get_appointment(db,appointment_id)
 
 @router.post("")
-def create_appointment(appointment:CreateAppointment,db:Session = Depends(get_db)):
-    db_appointment = Appointment(**appointment.model_dump())
-    db.add(appointment)
-    db.commit()
-    db.refresh(db_appointment)
-    return db_appointment
+def create_appointment(db:Session = Depends(get_db),appointment:CreateAppointment):
+    return create_appointment(db,appointment)
  
 @router.put("/{appointment_id}")
-def update_appointment(appointment_id:int,db:Session = Depends(get_db)):
-   return db.query(Appointment).filter(Appointment.appointment_id == appointment_id).first()
+def update_existing_appointment(db:Session = Depends(get_db),appointment_id:int,appointment:UpdateAppointment):
+   update_appointment = appointments.update_appointment(
+      db,
+      appointment_id=,
+      appointment)
+   return appointments.update_appointment(db,appointment_id)
 
 @router.delete("/{appointment_id}")
 def delete_appointment(appointment_id:int,db:Session = Depends(get_db)):
-   return
+   if not appointment_id:
+      raise HTTPException(status_code=404,detail='Appointment not found')
+   
+   return 
