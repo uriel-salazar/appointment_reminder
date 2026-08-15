ad94f7f2a12f47bc1727625592e00e415237a5c0
uriel-salazar <urielsalazar.dev@gmail.com>
2026-08-15 12:21:37 -0600

    Syntax error problem

--- app/routers/patients.py
+++ app/routers/patients.py
@@ -4,6 +4,9 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 from models.patient import Patient
 from schemas import CreatePatient,UpdatePatient
+
+from crud.patients import create_patient as crud_create_patient
+from crud.patients import delete_patient as crud_delete_patient
 from database.db import get_db
 router = APIRouter()
 
@@ -38,11 +41,15 @@ def update_patient(patient_id:int,patient_data:UpdatePatient,db:Session = Depend
         db.commit()
     except IntegrityError:
         db.rollback()
-        raise HTTPException(status_code=409, detail="Patient with this cellphone number already exists")
+        raise HTTPException(status_code = 409, detail="Patient with this cellphone number already exists")
     db.refresh(patient)
     return patient
     
     
-    
+@router.delete("/{patient_id}")
+def delete_patient(db: Session = Depends(get_db), patient_id:int ):
+    patient = crud_delete_patient(db,patient_id)
+    if not patient:
+        raise HTTPException(status_code = 404,detail =" Patient not found ")
 
 
